48b16f3b065222aec10248361d4e54ca1ea6ac16
izelfuruncuoglu <izel_forever@hotmail.com>
2014-05-03 02:37:24 +0300

    Update thing.js

--- thing.js
+++ thing.js
@@ -3,101 +3,90 @@
 
     var Thing = function (o) {
         return new t(o);
-    };
-
-    var t = function (o) {
-        this.toObject = function () { return o; }
     },
-    i = new Function(),
-    ins = new i();
+        t = function (o) {
+            this.toObject = function () {
+                return o;
+            }
+        },
+        contains = function (arr, elem) {
+            var i = arr.length;
 
-    i.prototype.contains = function (arr, elem) {
-        var i = arr.length;
+            while (i--)
+                if (arr[i] === elem)
+                    return true;
 
-        while (i--)
-            if (arr[i] === elem)
-                return true;
+            return false;
 
-        return false;
+        };
 
-    };
+    t.prototype = {
 
-    t.prototype.loop = function (fn) {
-        var obj = this.toObject();
+        loop: function (fn) {
+            var obj = this.toObject();
 
-        for (var key in obj) {
-            if (obj.hasOwnProperty(key))
-                fn.call(obj, key, obj[key]);
-        }
-        return this;
-    };
-
-    t.prototype.keys = function () {
-        var arr = [];
-        this.loop(function (k, v) {
-            arr.push(k);
-        });
-        return arr;
-    };
-
-    t.prototype.values = function () {
-        var arr = [];
-        this.loop(function (k, v) {
-            arr.push(v);
-        });
-        return arr;
-    };
-
-    t.prototype.get = function (keys) {
-
-        keys = new Array(keys);
-
-        var arr = [];
-
-        this.loop(function (k, v) {
-            if (ins.contains(keys, k))
+            for (var key in obj) {
+                if (obj.hasOwnProperty(key))
+                    fn.call(obj, key, obj[key]);
+            }
+            return this;
+        },
+        keys: function () {
+            var arr = [];
+            this.loop(function (k, v) {
+                arr.push(k);
+            });
+            return arr;
+        },
+        values: function () {
+            var arr = [];
+            this.loop(function (k, v) {
                 arr.push(v);
-        });
+            });
+            return arr;
+        },
+        get: function (keys) {
 
-        return arr;
+            keys = keys.match ? new Array(keys) : keys;
+
+            var arr = [];
+
+            this.loop(function (k, v) {
+                if (contains(keys, k))
+                    arr.push(v);
+            });
+
+            return arr;
+        },
+        set: function (o) {
+            var obj = this.toObject();
+
+            Thing(o).loop(function (k, v) {
+                obj[k] = v;
+            });
+
+            return this;
+
+        },
+        add: function (k, v) {
+            this.toObject()[k] = v;
+            return this;
+        },
+        containsKey: function (key) {
+            return contains(this.keys(), key);
+        },
+        clone: function () {
+            var o = {};
+            this.loop(function (k, v) {
+                o[k] = v;
+            });
+            return Thing(o);
+        }
+
     };
 
-    t.prototype.set = function (o) {
-        var obj = this.toObject();
+    Thing.contains = contains;
 
-        Thing(o).loop(function (k, v) {
-            obj[k] = v;
-        });
-
-        return this;
-
-    };
-
-    t.prototype.add = function (k, v) {
-        this.toObject()[k] = v;
-        return this;
-    };
-
-    t.prototype.containsKey = function (key) {
-        return ins.contains(this.keys(), key);
-    };
-
-    t.prototype.clone = function () {
-        var o = {};
-        this.loop(function (k, v) {
-            o[k] = v;
-        });
-        return Thing(o);
-    };
-
-    window.Thing = (function (thing) {
-
-        thing(ins.__proto__).loop(function (k, v) {
-            thing[k] = v;
-        });
-
-        return thing;
-    })(Thing);
-
+    window.Thing = Thing;
 
 })(window, document);
